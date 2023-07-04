c9de011a93b327d3206bcaff3f961db6dcba4191
bissixp <fernandosbissi18@hotmail.com>
2023-07-04 19:06:41 -0300

    feat: added vite on project and minor refactor in some funcs.

--- app/front-end/src/services/requests.tsx
+++ app/front-end/src/services/requests.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const apiPort = '3001';
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
+  baseURL: `http://localhost:${apiPort}`,
 });
 
 export const setToken = (token: string): void => {
