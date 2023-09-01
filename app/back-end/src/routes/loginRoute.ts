f3f8869ea5a02e46ca4a57450788a6da01d44e7d
bissixp <fernandosbissi18@hotmail.com>
2023-09-01 15:05:12 -0300

    fix: total clear in cache after logout

--- app/back-end/src/routes/loginRoute.ts
+++ app/back-end/src/routes/loginRoute.ts
@@ -8,7 +8,7 @@ loginRoute.post('/', UserController.login);
 loginRoute.get('/validate', UserController.authentication);
 loginRoute.get('/email/:searchParam', UserController.getUserEmail);
 loginRoute.get('/logoff', (_req, res) => {
-  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
   UserController.logout(_req, res);
 });
 
