90378bf4e539e8bf772e990918bac0b74ed91c24
bissixp <fernandosbissi18@hotmail.com>
2023-09-19 12:22:00 -0300

    feat: added CSS in perfil page

--- app/front-end/src/pages/perfil.tsx
+++ app/front-end/src/pages/perfil.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import Header from "../components/header";
 import { Link } from 'react-router-dom';
 import pastaSoTastyContext from '../context/context';
+import pastaSoTastyCard from '../images/pasta-so-tasty-card.png';
+import approveCard from '../images/approve-recipe.png';
+import pedingCard from '../images/peding-recipe.png';
 import '../styles/pages/perfil.css';
 
 const Perfil: React.FC = () => {
@@ -20,12 +23,29 @@ const Perfil: React.FC = () => {
     <>
       <Header isUserLoggedIn={logged} fullName={fullName}>
       </Header>
-      <Link to="/minhas-receitas" className="header-button">Minhas Receitas</Link>
-      {role === 'admin' ? (
-        <Link to="/aprovar-receitas" className="header-button">Aprovar receitas</Link>
-      ) : (
-        <Link to="/aguardando-aprovação" className="header-button">Aguardando Aprovação</Link>
-      )}
+      <div className="user-perfil">
+        <div className="card-container">
+          <img src={pastaSoTastyCard} alt="pasta-card" width="300" height="300" />
+          <Link to="/minhas-receitas" className="link-name">
+            Minhas Receitas
+          </Link>
+        </div>
+        {role === 'admin' ? (
+          <div className="card-container">
+            <img src={approveCard} alt="pasta-card" width="300" height="300" />
+            <Link to="/aprovar-receitas" className="link-name">
+              Aprovar receitas
+            </Link>
+          </div>
+        ) : (
+          <div className="card-container">
+            <img src={pedingCard} alt="pasta-card" width="100" height="100" />
+            <Link to="/aguardando-aprovação" className="link-name">
+              Aguardando Aprovação
+            </Link>
+          </div>
+        )}
+      </div>
     </>
   );
 };
