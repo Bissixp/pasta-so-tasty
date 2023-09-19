af14b0519671d03c61042d5c785b4e7448f3dbe7
bissixp <fernandosbissi18@hotmail.com>
2023-09-19 15:06:47 -0300

    feat: added CSS in login page

--- app/front-end/src/pages/login.tsx
+++ app/front-end/src/pages/login.tsx
@@ -2,11 +2,14 @@ import React, { useState, FormEvent, useContext, useEffect } from 'react';
 import { fetchLogin } from '../services/requests';
 import { useNavigate } from 'react-router-dom';
 import pastaSoTastyContext from '../context/context';
+import '../styles/pages/login.css';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [failedTryLogin, setFailedTryLogin] = useState<boolean>(false);
+  const [errorEmail, setErrorEmail] = useState<boolean>(false);
+  const [errorPassword, setErrorPassword] = useState<boolean>(false);
+  const [emptyInput, setEmptyInput] = useState<boolean>(false);
 
   const { fullName } = useContext(pastaSoTastyContext);
 
@@ -25,47 +28,83 @@ const Login: React.FC = () => {
       await fetchLogin({ email, password });
       navigate('/');
       window.location.reload();
-    } catch (error) {
-      setFailedTryLogin(true);
+    } catch (error: any) {
+      if (error.response) {
+        const { status, data } = error.response;
+
+        if (status === 401 && data.message === 'Incorrect email') {
+          setErrorPassword(false);
+          setEmptyInput(false);
+          setErrorEmail(true);
+        } else if (status === 401 && data.message === 'Incorrect password') {
+          setErrorEmail(false);
+          setEmptyInput(false);
+          setErrorPassword(true);
+        } else if (status === 400 && data.message === 'All fields must be filled') {
+          setErrorEmail(false);
+          setErrorPassword(false);
+          setEmptyInput(true);
+        }
+      }
     }
-  };
+  }
 
   return (
-    <>
-      <section className="user-login-area">
+    <section className="user-login-area">
+      <div className="login-card">
         <form onSubmit={(event) => login(event)}>
           <h1>Área do usuário</h1>
-          <label htmlFor="email-input">
-            Email:
-            <input
-              id="email-input"
-              className="login__login_input"
-              type="text"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              placeholder="Login"
-            />
-          </label>
-          <label htmlFor="password-input">
-            Senha:
-            <input
-              id="password-input"
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              placeholder="Senha"
-            />
-          </label>
-          {failedTryLogin && (
+          <div className="input-container">
+            <label htmlFor="email-input">
+              Email:
+              {' '}
+              <input
+                id="email-input"
+                type="text"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="Email"
+              />
+            </label>
+          </div>
+          <div className="input-container">
+            <label htmlFor="password-input">
+              Senha:
+              {' '}
+              <input
+                id="password-input"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                placeholder="Senha"
+              />
+            </label>
+          </div>
+          {errorEmail && (
             <p>
-              O endereço de e-mail ou a senha não estão corretos. Por favor,
-              tente novamente.
+              Endereço de e-mail não localizado.
             </p>
           )}
-          <button type="submit">Entrar</button>
+          {errorPassword && (
+            <p>
+              A senha está incorreta, por favor, tente novamente.
+            </p>
+          )}
+          {emptyInput && (
+            <p>
+              Informe o e-mail e a senha.
+            </p>
+          )}
+          <button type="submit" className='btn-visu'  >Entrar</button>
+          <h4>
+            Quero me cadastrar,{' '}
+            <a href="http://localhost:3000/registro" className="login-color">
+              Cadastrar
+            </a>
+          </h4>
         </form>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
