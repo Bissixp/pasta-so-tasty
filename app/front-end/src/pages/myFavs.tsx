1aca8fc2961638a306ca42ce807b0a05fd135261
bissixp <fernandosbissi18@hotmail.com>
2023-09-19 11:04:36 -0300

    feat: added css to favs page

--- app/front-end/src/pages/myFavs.tsx
+++ app/front-end/src/pages/myFavs.tsx
@@ -35,26 +35,30 @@ const MyFavs: React.FC = () => {
     <div>
       <Header isUserLoggedIn={logged} fullName={fullName}>
       </Header >
-      <h1>Meus Favoritos</h1>
-      {recipes.length > 0 ? (
-        recipes.map((recipe: IRecipe) => (
-          <div key={recipe.recipe_name}>
-            <Link to={`/receita/${recipe.id}-${recipe.recipe_name.split(' ').join('-')}`}
-            >
-              <h3>{recipe.recipe_name}</h3>
-            </Link>
-            {
-              recipe.recipe_photo.toLowerCase().startsWith('http') ? (
-                <img src={recipe.recipe_photo} alt={recipe.recipe_name} width="200" height="150" />
-              ) : (
-                <ImageLoader photo={recipe.recipe_photo} alt={recipe.recipe_name} />
-              )
-            }
+      <div className="home-recipes">
+        <h1>Meus Favoritos</h1>
+        {recipes.length > 0 ? (
+          <div className="recipe-list">
+            {recipes.map((recipe: IRecipe) => (
+              <div key={recipe.recipe_name} className="recipe-card">
+                {
+                  recipe.recipe_photo.toLowerCase().startsWith('http') ? (
+                    <img src={recipe.recipe_photo} alt={recipe.recipe_name} width="200" height="150" />
+                  ) : (
+                    <ImageLoader photo={recipe.recipe_photo} alt={recipe.recipe_name} width={"200"} height={"150"} />
+                  )
+                }
+                <Link to={`/receita/${recipe.id}-${recipe.recipe_name.split(' ').join('-')}`}
+                >
+                  <h3>{recipe.recipe_name}</h3>
+                </Link>
+              </div>
+            ))}
           </div>
-        ))
-      ) : (
-        <p>Nenhuma receita adicionada aos favoritos</p>
-      )}
+        ) : (
+          <p>Nenhuma receita adicionada aos favoritos</p>
+        )}
+      </div >
     </div >
   );
 };
